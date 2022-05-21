3ebd1906a52df525f884c0fae7ca729b16c8e33f
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-05-21 19:17:30 +0530

    Telegraph-Uploader-Bot

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Telegraph-Uploader-Bot
+
+![photo-1636743094110-5e153f93ad7e](https://user-images.githubusercontent.com/95665347/169654169-cef12501-658d-4b9d-b868-6de7623c54c7.jpeg)
+
+A simple media or file to telegraph link uploader bot.
+
+### Deploy
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/DARKEMPIRESL/Telegraph-Uploader-Bot)
+
+
+### Credits 
+
+• Thanks to [Dan](https://github.com/delivrance) for his [Pyrogram](https://pyrogram.org)
+
+• [𝕯𝖆𝖗𝖐 𝕰𝖒𝖕𝖎𝖗𝖊](https://github.com/DARKEMPIRESL) ``me``
---
+++ translation.py
@@ -0,0 +1,34 @@
+class Translation(object):
+    START_TEXT = """
+Hai,{}
+
+I'm a simple Telegraph Uploader bot💯
+
+I can convert gif, image or video(Mp4only) into telegra.ph links
+
+Click /help for more details...
+
+You must subscribe our channel in order to use me😇
+"""
+    HELP_USER = """
+<b><u>Help and Informations</u></b>
+
+➠ Just give me a media under 5MB.
+➠ Then I will download it.
+➠ I will then upload it to the `Telegra.ph` link.
+"""
+
+    ABOUT_TEXT = """
+<b><u>Informations About Me</u></b>
+
+⭕️My Name : Telegraph BOT
+        
+⭕️Creater : @ImDark_Empire
+
+⭕️Language : Python3
+
+⭕️Library : Pyrogram asyncio 0.16.1
+"""
+    DOWNLOAD_TEXT = "<code>Downloading to My Server ...</code>"
+    UPLOADING_TEXT = "<code>Downloading Completed. Now I'am Uploading to telegra.ph Link ...</code>"
+    SOMETHING_WRONG = "Something Wrong! Click help button for more..."
